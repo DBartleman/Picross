11723313b8b54cbdd1f7aba2523a1c88ce8a8fa7
dbartleman <drewbartleman@gmail.com>
2020-11-23 21:31:40 -0800

    getting the buttons correct

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 
 function App() {
   function reveal(e) {
-    e.preventDefault();
-    console.log('The button was clicked.');
+    //e.preventDefault();
+    e.target.className = "tileButtonFilled tileButton";
+    e.target.disabled = true;
+    //console.log('The button was clicked.');
+  }
+  function reset(e) {
+    //e.preventDefault();
+    let buttons = document.querySelectorAll("tileButton");
+    for(let i = 0; i < buttons.length; i++) {
+      buttons[i].className = "tileButtonDefault tileButton";
+      buttons[i].disabled = false;
+    }
+    //console.log('The button was clicked.');
   }
 
   return (
@@ -14,11 +25,12 @@ function App() {
         </header>
         <div className="gameBoard">
           game board (testing)
-          <button onClick={reveal} class="tileButtonDefault"> </button>
-          <button onClick={reveal} class="tileButtonDefault"> </button>
-          <button onClick={reveal} class="tileButtonDefault"> </button>
-          <button onClick={reveal} class="tileButtonDefault"> </button>
-          <button onClick={reveal} class="tileButtonDefault"> </button>
+          <button onClick={reset}> reset </button>
+          <button onClick={reveal} class="tileButtonDefault tileButton"> </button>
+          <button onClick={reveal} class="tileButtonDefault tileButton"> </button>
+          <button onClick={reveal} class="tileButtonDefault tileButton"> </button>
+          <button onClick={reveal} class="tileButtonDefault tileButton"> </button>
+          <button onClick={reveal} class="tileButtonDefault tileButton"> </button>
         </div>
       </div>
     </div>
