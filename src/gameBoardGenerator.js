b768267c7ea675c39858805febe3c7d8d4ce0011
dbartleman <drewbartleman@gmail.com>
2020-12-08 23:48:20 -0800

    experimental button grid testing

--- src/gameBoardGenerator.js
+++ src/gameBoardGenerator.js
@@ -6,37 +6,15 @@ function GameBoardGenerator(grid) {
         e.target.disabled = true;
         //console.log('The button was clicked.');
     }
+    let middle = [];
+    for(let i = 0; i < 6; i++) {
+        middle.push(<button onClick={reveal} className="tileButtonDefault tileButton"> </button>);
+    }
     //TODO: programatically render a grid of various size depending on props sent
+    // <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
     return (
         <div className="gameBoard">
-            <div>
-                {/* <p>1 1 1</p> */}
-                {/* <p>1 1 1</p> */}
-            </div>
-            <div>
-                {/* <p>1 1 1</p> */}
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-            </div>
-            <div>
-                {/* <p>1 1 1 1 1 1</p> */}
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-            </div>
-            <div>
-                {/* <p>1</p> */}
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-                <button onClick={reveal} className="tileButtonDefault tileButton"> </button>
-            </div>
+            {middle}
         </div>
     );
 }
